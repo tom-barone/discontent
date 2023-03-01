7028ee6342cc497e22e23eb97fb72a700b9b3378
Tom Barone <mail@tombarone.net>
2023-03-01 19:50:42 +0100

    Add integration tests for both firefox & chrome

--- integration_tests/setup.rb
+++ integration_tests/setup.rb
@@ -1,30 +1,62 @@
 # frozen_string_literal: true
 
 require 'capybara/minitest'
+require 'date'
 require 'selenium-webdriver'
-require 'minitest/hooks/test'
 
 class CapybaraTestCase < Minitest::Test
   include Capybara::DSL
   include Capybara::Minitest::Assertions
-  include Minitest::Hooks
 
+  # Chrome setup
   chrome_options = Selenium::WebDriver::Chrome::Options.new
-
   chrome_options.add_argument(
     '--disable-extensions-except=../extension/dist/chrome'
   )
   chrome_options.add_argument('--load-extension=../extension/dist/chrome')
-  chrome_options.add_argument('--headless=new')
+  chrome_options.add_argument('--headless=new') if ENV['HEADLESS'] == 'true'
   Capybara.register_driver :chrome do |app|
     Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+
+  # Firefox setup
+  firefox_options = Selenium::WebDriver::Firefox::Options.new
+  firefox_options.add_argument('-headless') if ENV['HEADLESS'] == 'true'
+  Capybara.register_driver :firefox do |app|
+    driver = Capybara::Selenium::Driver.new(app, browser: :firefox, options: firefox_options)
+    driver.browser.install_addon('../extension/dist/firefox', true)
+    sleep(1) # Give the extension a second to install
+    driver
   end
 
   Capybara.default_driver = :chrome
   Capybara.default_max_wait_time = 20 # Seconds
   Capybara.enable_aria_label = true
 
+  def extension_popup_url
+    case Capybara.current_driver
+    when :chrome
+      return "chrome-extension://#{ENV['CHROME_EXTENSION_ID']}/menu/menu.html"
+    when :firefox
+      prefs_file = "#{page.driver.browser.capabilities['moz:profile']}/prefs.js"
+      # Extract the webextensions uuids
+      uuid_regex = /user_pref\("extensions.webextensions.uuids", "(.+)"\)/
+      uuid_match = uuid_regex.match(File.read(prefs_file))[1]
+      # Remove all the backslashes
+      uuid_match.gsub!('\\', '')
+      # Parse the uuid_match as JSON
+      uuids = JSON.parse(uuid_match)
+      return "moz-extension://#{uuids[ENV['FIREFOX_EXTENSION_ID']]}/menu/menu.html"
+    end
+    throw 'Unknown driver'
+  end
+
   def teardown
+    # Take a screenshot if the test failed
+    timestamp = Time.now.strftime('%Y_%m_%d-%H_%M_%S')
+    filename = "#{name}-#{timestamp}.png"
+    save_screenshot("ci/screenshots/#{filename}") unless passed?
+
     Capybara.reset_sessions!
     Capybara.use_default_driver
   end
--- integration_tests/test/extension_popup.rb
+++ integration_tests/test/extension_popup.rb
@@ -2,20 +2,142 @@
 
 require_relative '../setup'
 
+GOOD_ICON_SETTING_SELECTOR = 'input[data-settings-page-target="goodInput"]'
+CONTROVERSIAL_ICON_SETTING_SELECTOR = 'input[data-settings-page-target="controversialInput"]'
+BAD_ICON_SETTING_SELECTOR = 'input[data-settings-page-target="badInput"]'
+SPINNER_SELECTOR = '[data-icon-setting-target="spinner"]'
+TICK_SELECTOR = '[data-icon-setting-target="check"]'
+ERROR_SELECTOR = '[data-icon-setting-target="error"]'
+
 class TestExtensionPopup < CapybaraTestCase
-  def setup
-    extension_page = "chrome-extension://#{ENV['CHROME_EXTENSION_ID']}/menu/menu.html"
-    visit(extension_page)
+  def prepare(browser)
+    Capybara.current_driver = browser
+    visit(extension_popup_url)
   end
 
-  def test_the_popup_displays_correctly
-    assert_text('Discontent')
-    assert_no_text('Settings')
+  # Run tests for multiple browsers
+  %i[firefox chrome].each do |browser|
+    define_method("test_#{browser}_the_popup_displays_correctly") do
+      prepare(browser)
+      assert_text('Discontent')
+      assert_no_text('Settings')
+      assert_no_text('Settings')
+      assert_button('Upvote button')
+      assert_button('Downvote button')
+      assert_equal find_button('Upvote button').text, '💚'
+      assert_equal find_button('Downvote button').text, '💢'
+    end
+
+    define_method("test_#{browser}_can_show_and_hide_the_settings_page") do
+      prepare(browser)
+      click_on('Open settings')
+      assert_text('Settings')
+      assert_text('Good')
+      assert_text('Spicy')
+      assert_text('Bad')
+      assert_button('Reset')
+      assert_link('Icon list')
+      good_icon_input = find(GOOD_ICON_SETTING_SELECTOR)
+      controversial_icon_input = find(CONTROVERSIAL_ICON_SETTING_SELECTOR)
+      bad_icon_input = find(BAD_ICON_SETTING_SELECTOR)
+      assert_equal good_icon_input.value, '💚'
+      assert_equal controversial_icon_input.value, '🤨'
+      assert_equal bad_icon_input.value, '💢'
+
+      click_on('Close settings')
+      assert_no_text('Settings')
+      assert_no_text('Good')
+      assert_no_text('Spicy')
+      assert_no_text('Bad')
+      assert_no_button('Reset')
+      assert_no_link('Icon list')
+    end
+
+    define_method("test_#{browser}_changing_and_resetting_the_vote_settings") do
+      prepare(browser)
+      click_on('Open settings')
+      good_icon_input = find(GOOD_ICON_SETTING_SELECTOR)
+      controversial_icon_input = find(CONTROVERSIAL_ICON_SETTING_SELECTOR)
+      bad_icon_input = find(BAD_ICON_SETTING_SELECTOR)
+      assert_equal good_icon_input.value, '💚'
+      assert_equal controversial_icon_input.value, '🤨'
+      assert_equal bad_icon_input.value, '💢'
+      sleep(0.5) # enough for the open animation to finish
+
+      # Set new icons
+      good_icon_input.set('g')
+      controversial_icon_input.set('c')
+      bad_icon_input.set('b')
+
+      # Check that 3 checkmarks appear
+      assert_selector(TICK_SELECTOR, count: 3)
+
+      # After some time they should disappear
+      sleep(3)
+      assert_no_selector(TICK_SELECTOR)
+
+      assert_equal good_icon_input.value, 'g'
+      assert_equal controversial_icon_input.value, 'c'
+      assert_equal bad_icon_input.value, 'b'
+
+      # Check the vote buttons show the new icons
+      click_on('Close settings')
+      assert_equal find_button('Upvote button').text, 'g'
+      assert_equal find_button('Downvote button').text, 'b'
+
+      # Open settings again and reset
+      click_on('Open settings')
+      click_on('Reset')
+      assert_selector(TICK_SELECTOR, count: 3)
+      sleep(3)
+      assert_no_selector(TICK_SELECTOR)
+
+      assert_equal good_icon_input.value, '💚'
+      assert_equal controversial_icon_input.value, '🤨'
+      assert_equal bad_icon_input.value, '💢'
+      click_on('Close settings')
+      assert_equal find_button('Upvote button').text, '💚'
+      assert_equal find_button('Downvote button').text, '💢'
+    end
+
+    define_method("test_#{browser}_setting_the_vote_icons_to_invalid_values") do
+      prepare(browser)
+      click_on('Open settings')
+      good_icon_input = find(GOOD_ICON_SETTING_SELECTOR)
+      controversial_icon_input = find(CONTROVERSIAL_ICON_SETTING_SELECTOR)
+      bad_icon_input = find(BAD_ICON_SETTING_SELECTOR)
+      sleep(0.5) # enough for the open animation to finish
+
+      # Set invalid
+      good_icon_input.send_keys('gg')
+      controversial_icon_input.send_keys(:backspace)
+      bad_icon_input.send_keys('as')
+
+      # Check that 3 error messages show
+      assert_selector(ERROR_SELECTOR, count: 3)
+      # Check the error messages are correct
+      assert_selector('[title="Icon must be a single character"]', count: 3)
+
+      # Check they don't disappear
+      sleep(5)
+      assert_selector(ERROR_SELECTOR, count: 3)
+    end
+
+    define_method("test_#{browser}_clicking_the_icon_list_link") do
+      prepare(browser)
+      click_on('Open settings')
+      new_window = window_opened_by { click_link 'Icon list' }
+      within_window new_window do
+        assert_text('Full Emoji List, v15.0')
+      end
+    end
   end
 
-  def test_can_show_the_settings_page
-    assert_text('Discontent')
-    click_on('Open settings')
-    assert_text('Settings')
+  def teardown
+    # Run javascript to clear local storage
+    # Need to deal with annoying browser / chrome name differences
+    page.execute_script('let browser = window.browser || window.chrome; browser.storage.local.clear()')
+    sleep(1)
+    super
   end
 end
