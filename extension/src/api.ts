830b652dca96283d1b4ffea68c7c54b42189973a
Tom Barone <mail@tombarone.net>
2023-03-02 18:54:44 +0100

    Add handling of production / development environments to the extension

--- extension/src/api.ts
+++ extension/src/api.ts
@@ -1,19 +1,23 @@
 import { Score, ScoresRequest, ScoresResponse } from "./types";
 
+const PRODUCTION_ENDPOINT =
+  "https://5h7cyehfq1.execute-api.us-east-1.amazonaws.com/v1";
+const DEVELOPMENT_ENDPOINT =
+  "http://localhost:9000/lambda-url/request-handler/v1";
+
 export async function fetchScores(
   request: ScoresRequest
 ): Promise<ScoresResponse> {
   const params = new URLSearchParams();
 
   params.append("from", request.json);
-  // TODO: Add production / development handling here
-  const url =
-    "http://localhost:9000/lambda-url/request-handler/v1/scores?" + params;
+  const endpoint =
+    process.env.NODE_ENV === "production"
+      ? PRODUCTION_ENDPOINT
+      : DEVELOPMENT_ENDPOINT;
+  const url = endpoint + "/scores?" + params;
   const response = await fetch(url, {
     method: "GET",
-    headers: {
-      "content-type": "application/json",
-    },
   });
   const scores = await response.json();
 
